eca66b263134631d17a922b802ec746767b4ef01
Prashant Ghimire <prashant.ghimire@gmail.com>
2023-02-20 20:36:08 -0500

    Initial Commit

---
+++ SGA.AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SGA.AI
+//
+//  Created by Prashant Ghimire on 2023-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
